dcaab1270ac44f5cff8e4f7095eb6b747b94afd2
stephen <380233718@qq.com>
2018-04-15 21:09:07 +0800

    hello.go
    hello.go

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+	a := [...]int{3,48,3423,1,3,435}
+	fmt.Println(a)
+
+	num := len(a)
+	for i := 0; i < num; i++{
+		for j := i + 1;j < num; j++{
+			if a[i] > a[j]{
+				a[i] , a[j] = a[j] , a[i]
+			}
+		}
+	}
+	fmt.Println(a);
+
+	s1 := []int{1,2,3,4,5,6}
+	s2 := []int{7,8,9,1,1,1,1,1,1,1,1}
+	copy(s2[2:4],s1[1:3])
+	fmt.Println(s2)
+}
+
